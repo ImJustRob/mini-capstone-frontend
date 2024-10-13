3424e91cb99fc33143bf2bd37f38ffab9dec0c7e
ImJustRob <robertalothian@hotmail.com>
2024-10-13 14:19:13 -0400

    search filter

--- src/ProductsIndex.jsx
+++ src/ProductsIndex.jsx
@@ -1,9 +1,12 @@
+import { useState } from "react";
     export function ProductsIndex(props) {
+      const [searchFilter, setSearchFilter] = useState("");
       console.log(props.products)
       return (
         <div>
           <h1>All products</h1>
-         {props.products.map((product) => (
+          Search filter: <input type="text" value={searchFilter} onChange={(event) => setSearchFilter(event.target.value)} />
+          {props.products.filter((product) => product.name.toLowerCase().includes(searchFilter.toLowerCase())).map(product => (
            <div key={product.id}>
             <p>{product.name}</p>
             <p>{product.price}</p>
