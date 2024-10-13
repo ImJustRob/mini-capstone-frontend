0309466363a9deb690f95698c4250fe61a3ce5cb
ImJustRob <robertalothian@hotmail.com>
2024-10-13 12:57:50 -0400

    basic signup/logout display logic

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,17 +1,29 @@
 import { Link } from "react-router-dom";
+import { LogoutLink } from "./LogoutLink";
 
 
 export function Header() {
+  let authenticationLinks;
+  if (localStorage.jwt === undefined) {   
+    authenticationLinks = (
+      <div>
+        <a href="/login">Login</a> | 
+        <a href="/signup">Signup</a>
+      </div>
+    )
+  }  else {  
+    authenticationLinks = (
+      <LogoutLink />
+    )
+  }
     return (
       <header>
         <nav>
-        <a href="/">Home</a> | <a href="/login">Login</a> |
-        <Link to="/signup">Signup</Link> |
-        <Link to="/login">Login</Link> |
-        <Link to="/products/new">New Product</Link> |
-        <Link to="/products"> Products</Link> |
+        <Link to="/products/new">New Product</Link> | 
+        <Link to="/products"> Products</Link> | 
         <Link to="/cart"> Cart</Link>
         </nav>
+        {authenticationLinks}
       </header>
     )
   }
--- src/SignupPage.jsx
+++ src/SignupPage.jsx
@@ -3,24 +3,25 @@ import { useState } from "react";
 
 export function SignupPage() {
   const [errors, setErrors] = useState([]);
-
+  const [name, setName] = useState("");
+  
   const handleSubmit = (event) => {
     event.preventDefault();
     setErrors([]);
     const params = new FormData(event.target);
     axios
-      .post("http://localhost:3000/users.json", params)
-      .then((response) => {
-        console.log(response.data);
-        event.target.reset();
-        window.location.href = "/"; // Change this to hide a modal, redirect to a specific page, etc.
-      })
-      .catch((error) => {
-        console.log(error.response.data.errors);
-        setErrors(error.response.data.errors);
-      });
+    .post("http://localhost:3000/users.json", params)
+    .then((response) => {
+      console.log(response.data);
+      event.target.reset();
+      window.location.href = "/"; // Change this to hide a modal, redirect to a specific page, etc.
+    })
+    .catch((error) => {
+      console.log(error.response.data.errors);
+      setErrors(error.response.data.errors);
+    });
   };
-
+  
   return (
     <div id="signup">
       <h1>Signup</h1>
@@ -31,7 +32,12 @@ export function SignupPage() {
       </ul>
       <form onSubmit={handleSubmit}>
         <div>
-          Name: <input name="name" type="text" />
+          Name: <input  name="name" 
+            type="text" 
+            value={name} 
+            onChange={(event) => setName(event.target.value)} 
+            />
+          <small>{20 - name.length} characters remaining</small>
         </div>
         <div>
           Email: <input name="email" type="email" />
